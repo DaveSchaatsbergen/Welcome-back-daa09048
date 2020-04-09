0a519213c11138548b3f589267dbd24c1101bcf0
Dave Schaatsbergen <45204337+DaveSchaatsbergen@users.noreply.github.com>
2020-04-09 11:17:58 +0200

    Add files via upload

---
+++ scale.php
@@ -0,0 +1,10 @@
+<?php
+$links = 5;
+$rechts = 5;
+if($links == $rechts) {
+    echo "In belans" . "<br>";
+    echo "Waardes zijn: Links = " .$links. " rechts = " .$rechts;
+} else {
+    echo "Niet in belans";
+    echo "Waardes zijn: Links = " .$links. " rechts = " .$rechts;
+}
