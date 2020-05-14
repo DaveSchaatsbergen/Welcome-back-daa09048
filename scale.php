52c74480218824f867c7dbc2cff41dd6b5257920
Dave Schaatsbergen <45204337+DaveSchaatsbergen@users.noreply.github.com>
2020-05-14 12:03:32 +0200

    verbeteringen

--- scale.php
+++ scale.php
@@ -2,9 +2,9 @@
 $links = 5;
 $rechts = 5;
 if ($links == $rechts) {
-    echo "In belans";
-    echo "Waardes zijn: Links = " . $links . " rechts = " . $rechts;
+	echo $links . " " . $rechts;
+    echo "In balans";
 } else {
+	echo $links . " " . $rechts;
     echo "Niet in belans";
-    echo "Waardes zijn: Links = " . $links . " rechts = " . $rechts;
 }
